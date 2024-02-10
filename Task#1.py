868bffc70150fd7ade7f159f94edb7cea9ec0270
Nickolay Grigoriev <gnarus@inbox.ru>
2024-02-10 12:14:15 +0300

    second commit

--- Task#1.py
+++ Task#1.py
@@ -1,4 +1,6 @@
 import functools
+import json
+
 import requests
 import pytest
 import yaml
@@ -22,12 +24,30 @@ def get_post(token):
     return resource.json()
 
 
-def list_of_id():
+def find_id():
     result = get_post()
-    lst = []
+    flag = False
     for item in result['data']:
         print(item)
+        if 97820 == item['id']:
+            flag = True
+    return flag
+#
+# def test_id():
+#     assert find_id(), 'Faile Test-id'
 
+@decorator_loggin
+def generate_post(token):
+    in_post = {"title": "1", "description": "11", "content": "111"} # передаваемые данные
+    auth_post = [('username', data['username']), ('password', data['passwd']),( "title", "1"), ("description", "11"), ("content", "111")]
+    auth_post1 = [('username', data['username']), ('password', data['passwd'])]
+    # передачаа поста (передается токкен регистрации и данные)
+    response_post = requests.post(data['url_post'], headers={"X-Auth-Token": token}, data=in_post)
+    answer1 = response_post.status_code
+    print(answer1)
 
-print(get_post())
-print(list_of_id())
+    return response_post.json()
+
+print(find_id())
+print(generate_post())
+
